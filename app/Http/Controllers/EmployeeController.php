842f8f1fec726c070c4e47bf5a64d4305ed74690
maruf3535 <marufrafsan677@gmail.com>
2023-01-15 01:01:46 +0600

    First day complete

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function addEmployee()
+    {
+        return "I am in addEmployee func";
+    }
+}
