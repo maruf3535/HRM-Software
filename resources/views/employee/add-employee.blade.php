e0352608e52ad15c38d42832a9aac7fc6647f1dd
maruf3535 <marufrafsan677@gmail.com>
2023-01-19 01:42:52 +0600

    nothing is improved

--- resources/views/employee/add-employee.blade.php
+++ resources/views/employee/add-employee.blade.php
@@ -35,7 +35,7 @@
                 <div class="row">
                     <div class="col-12 col-md-6 col-lg-6">
                         <div class="card">
-                            <form action="{{route('employee.basic.information.submit')}}" method="post">
+                            <form action="{{ route('employee.basic.information.submit') }}" method="post">
                                 @csrf
                                 <div class="card-header">
                                     <h4>Basic Information</h4>
@@ -43,16 +43,20 @@
                                 <div class="card-body">
                                     <div class="form-group">
                                         <label>First Name <span class="required-star">*</span> </label>
-                                        <input type="text" class="form-control" name="first_name" maxlength="35" required>
+                                        <input type="text" class="form-control" name="first_name" maxlength="35"
+                                            required>
                                     </div>
+                                    
                                     <div class="form-group">
                                         <label>Middle Name</label>
                                         <input type="text" class="form-control" name="middle_name" maxlength="35">
                                     </div>
+
                                     <div class="form-group">
                                         <label>Last Name</label>
                                         <input type="text" class="form-control" name="last_name" maxlength="35">
                                     </div>
+
                                     <div class="form-group">
                                         <label>Nick Name</label>
                                         <input type="text" class="form-control" name="nickname" maxlength="35">
@@ -60,11 +64,12 @@
 
                                     <div class="form-group">
                                         <label>Email</label>
-                                        <input type="email" class="form-control" name="email"  maxlength="255">
+                                        <input type="email" class="form-control" name="email" maxlength="255">
                                     </div>
+
                                     <div class="form-group">
                                         <label>Alternative Email</label>
-                                        <input type="email" class="form-control" name="alternative_email"  maxlength="255">
+                                        <input type="email" class="form-control" name="alternative_email" maxlength="255">
                                     </div>
 
                                     <div class="form-group">
@@ -82,7 +87,6 @@
                                         <input type="date" class="form-control" name="date_of_birth" required>
                                     </div>
 
-
                                     <div class="form-group">
                                         <label>Mobile Number <span class="required-star">*</span></label>
                                         <div class="input-group">
@@ -91,10 +95,11 @@
                                                     <i class="fas fa-phone"></i>
                                                 </div>
                                             </div>
-                                            <input type="text" class="form-control phone-number" name="mobile_number" maxlength="15" required>
+                                            <input type="text" class="form-control mobile-number number-input"
+                                                name="mobile_number" maxlength="15" required>
                                         </div>
+                                        <span>Only number!</span>
                                     </div>
-
 
                                     <div class="form-group">
                                         <label>Phone Number</label>
@@ -104,8 +109,10 @@
                                                     <i class="fas fa-phone"></i>
                                                 </div>
                                             </div>
-                                            <input type="text" class="form-control phone-number"  name="phone_number" maxlength="15">
+                                            <input type="text" class="form-control phone-number number-input"
+                                                name="phone_number" maxlength="15">
                                         </div>
+                                        <span>Only number!</span>
                                     </div>
                                 </div>
                                 <div class="card-footer text-right">
@@ -114,17 +121,160 @@
                             </form>
                         </div>
 
-                        
+
                     </div>
-                    
+                    <div class="col-12 col-md-6 col-lg-6">
+                        <div class="card">
+                            <form action="{{ route('employee.basic.information.submit') }}" method="post">
+                                @csrf
+                                <div class="card-header">
+                                    <h4>Employee Information</h4>
+                                </div>
+                                <div class="card-body">
+
+                                    <div class="form-group">
+                                        <label>Department <span class="required-star"><button id="modal-login-part" class="badge badge-primary" style="cursor: pointer;">Set</button></span></label>
+                                        <select class="form-control select2" name="gender" required>
+                                            <option value="-1">Please Select One</option>
+                                            <option value="1">Male</option>
+                                            <option value="2">Female</option>
+                                            <option value="9">Other</option>
+                                        </select>
+                                    </div>
+
+                                    {{-- <button class="btn btn-primary" id="modal-login-part">Login</button> --}}
+
+                        
+
+
+
+                                    <div class="form-group">
+                                        <label>First Name <span class="required-star">*</span> </label>
+                                        <input type="text" class="form-control" name="first_name" maxlength="35"
+                                            required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Middle Name</label>
+                                        <input type="text" class="form-control" name="middle_name" maxlength="35">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Last Name</label>
+                                        <input type="text" class="form-control" name="last_name" maxlength="35">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Nick Name</label>
+                                        <input type="text" class="form-control" name="nickname" maxlength="35">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="email" class="form-control" name="email" maxlength="255">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Alternative Email</label>
+                                        <input type="email" class="form-control" name="alternative_email" maxlength="255">
+                                    </div>
+
+                                    
+
+                                    <div class="form-group">
+                                        <label>Date of Birth <span class="required-star">*</span></label>
+                                        <input type="date" class="form-control" name="date_of_birth" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Mobile Number <span class="required-star">*</span></label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <i class="fas fa-phone"></i>
+                                                </div>
+                                            </div>
+                                            <input type="text" class="form-control mobile-number number-input"
+                                                name="mobile_number" maxlength="15" required>
+                                        </div>
+                                        <span>Only number!</span>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Phone Number</label>
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">
+                                                    <i class="fas fa-phone"></i>
+                                                </div>
+                                            </div>
+                                            <input type="text" class="form-control phone-number number-input"
+                                                name="phone_number" maxlength="15">
+                                        </div>
+                                        <span>Only number!</span>
+                                    </div>
+                                </div>
+                                <div class="card-footer text-right">
+                                    <button class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+
+                    </div>
+
                 </div>
             </div>
         </section>
+        <form class="modal-part" id="modal-login-part">
+            <p>This login form is taken from elements with <code>#modal-login-part</code> id.</p>
+            <div class="form-group">
+                <label>Username</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="fas fa-envelope"></i>
+                        </div>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Email" name="email">
+                </div>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="fas fa-lock"></i>
+                        </div>
+                    </div>
+                    <input type="password" class="form-control" placeholder="Password" name="password">
+                </div>
+            </div>
+            <div class="form-group mb-0">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" name="remember" class="custom-control-input" id="remember-me">
+                    <label class="custom-control-label" for="remember-me">Remember Me</label>
+                </div>
+            </div>
+        </form>
     </div>
 @endsection
 
 @push('scripts')
     <!-- JS Libraies -->
-
+    <script>
+        // Mobile & Phone number input format validate
+        var numbers = document.querySelectorAll('.number-input');
+        // console.log(numbers);
+        numbers.forEach(elm => {
+            elm.addEventListener('input', () => {
+                elm.value = parseInt(elm.value)
+            })
+        });
+    </script>
     <!-- Page Specific JS File -->
+    <script src="{{ asset('library/prismjs/prism.js') }}"></script>
+
+    <script src="{{ asset('js/page/bootstrap-modal.js') }}"></script>
+
 @endpush
